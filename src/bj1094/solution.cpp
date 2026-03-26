c936c2fa30bb53f9c4bed69597ad4dbb79223e92
An Jongwon <ahn.jw.1116@gmail.com>
2026-03-26 16:33:23 +0900

    해결완료

--- src/bj1094/solution.cpp
+++ src/bj1094/solution.cpp
@@ -1,5 +1,38 @@
 #include "bj1094/solution.hpp"
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
 
 int Solution::solve(int X) {
-    return 1;
+
+    vector<int> makdae(1,64);
+
+    while(accumulate(makdae.begin(),makdae.end(),0)!=X){
+        sort(makdae.begin(), makdae.end());
+        int min = makdae[0];
+         vector<int> temp_v(makdae.size()-1,0);
+        for(int i =0; i<makdae.size()-1; ++i){
+            temp_v[i] = makdae[i+1];
+        }
+        makdae = temp_v;
+        int temp1 = (int)(min/2);
+        int temp2 = (int)(min/2);
+        if(temp1 + accumulate(makdae.begin(),makdae.end(),0)>=X){
+            makdae.push_back(temp1);
+        }
+        else{
+            makdae.push_back(temp1);
+            makdae.push_back(temp2);
+        }
+
+
+        // for(int i =0; i<makdae.size(); ++i){
+        //     cout << makdae[i] << " ";
+        // }
+        // cout << "\n";
+    }
+
+    return makdae.size();
 }
